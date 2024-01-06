de0d73a0ad32c84031fff2d84de19878e72fa876
Waldsson Sacramento <waldsson.dev@gmail.com>
2024-01-06 00:28:06 -0300

    Add initial project files and configurations

---
+++ src/controller/UserController.js
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+import { hash } from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+export default {
+  async createUser(req, res) {
+    try {
+      
+    } catch (error) {
+      
+    }
+  },
+
+}
