a14ddb41d6671b0759c5c7386faedea90f3d6236
Waldsson Sacramento <waldsson.dev@gmail.com>
2024-01-23 12:41:01 -0300

    Add Ticket and LuckyNumbers controllers to routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express"
 import UserController from './controller/UserController'
 import DrawController from './controller/DrawController'
+import TicketController from "./controller/TicketController"
+import LuckyNumbersController from "./controller/LuckyNumbersController"
 
 const router = Router()
 
@@ -26,8 +28,22 @@ router.route('/draw/:id')
   .get(DrawController.findDraw)
   .put(DrawController.updateDraw)
   .delete(DrawController.deleteDraw)
+  .post(DrawController.updateWinner)
 
 router.route("/draws")
   .get(DrawController.findAllDraws)
+
+router.route('/ticket')
+  .post(TicketController.createTicket)
+  .get(TicketController.findAllTickets)
+
+router.route('/ticket/:id')
+  .delete(TicketController.deleteTicket)
+  .get(TicketController.generateTickets)
+  .put(TicketController.reserveTicket)
+  .post(TicketController.sellTicket)
+
+router.route('/unreserve-ticket/:id')
+  .put(TicketController.unreserveTicket)
 
 export { router }
